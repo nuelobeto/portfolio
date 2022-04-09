4c3ffd589befdb3be77818ab7b93b46522e02320
Nuel Obeto <nuelobeto@gmail.com>
2022-04-09 06:35:14 +0100

    created about page and made some adjustments

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,43 @@
 import "./About.css";
+import aboutImg from "./images.png";
 
 function About() {
   return (
     <section className="about" id="about">
-      <h2>About</h2>
+      <div className="about_content section">
+        <h2 className="section_heading">About Me</h2>
+        <div className="about_body">
+          <div className="about_body-left">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+              vitae culpa amet! In officia quasi iure, necessitatibus cum
+              inventore voluptas?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+              quisquam ipsa tempora adipisci vero? Esse.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              recusandae magni iure nemo sed architecto incidunt distinctio
+              voluptate dicta temporibus?
+            </p>
+            <p>Here are a few technologies I’ve been working with recently:</p>
+            <ul>
+              <li>JavaScript (ES6+)</li>
+              <li>React js</li>
+              <li>Node js</li>
+              <li>Sanity</li>
+              <li>Firebase</li>
+              <li>Solidity</li>
+              <li>Web3 js</li>
+            </ul>
+          </div>
+          <div className="about_body-right">
+            <img src={aboutImg} alt="" />
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,7 +3,9 @@ import "./Projects.css";
 function Projects() {
   return (
     <section className="projects" id="projects">
-      <h2>Projects</h2>
+      <div className="projects_content section">
+        <h2 className="section_heading">Somethings I've built</h2>
+      </div>
     </section>
   );
 }
