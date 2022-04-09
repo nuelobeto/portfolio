1d2290cb9e6964c92594920d1dd86412fb6cd71f
Nuel Obeto <nuelobeto@gmail.com>
2022-04-09 15:11:39 +0100

    created projects page

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,103 @@
 import "./Projects.css";
+import { FiGithub, FiExternalLink } from "react-icons/fi";
+import project1 from "./project1.png";
 
 function Projects() {
   return (
     <section className="projects" id="projects">
       <div className="projects_content section">
-        <h2 className="section_heading">Somethings I've built</h2>
+        <h2 className="section_heading">Some Things I've Built</h2>
+        <ul className="projects_list">
+          <li className="project">
+            <div className="project_img">
+              <a href="projectlink">
+                <img src={project1} alt="" />
+              </a>
+            </div>
+
+            <div className="project_content">
+              <p className="featured_project">Featured Project</p>
+              <h3 className="project_title">Project Title</h3>
+              <p className="project_description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum,
+                in?
+              </p>
+              <ul className="tags">
+                <li>Vs Code</li>
+                <li>React</li>
+                <li>Node</li>
+              </ul>
+              <div className="links">
+                <a href="github">
+                  <FiGithub />
+                </a>
+                <a href="projectlink">
+                  <FiExternalLink />
+                </a>
+              </div>
+            </div>
+          </li>
+
+          <li className="project">
+            <div className="project_img">
+              <a href="projectlink">
+                <img src={project1} alt="" />
+              </a>
+            </div>
+
+            <div className="project_content">
+              <p className="featured_project">Featured Project</p>
+              <h3 className="project_title">Project Title</h3>
+              <p className="project_description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum,
+                in?
+              </p>
+              <ul className="tags">
+                <li>Vs Code</li>
+                <li>React</li>
+                <li>Node</li>
+              </ul>
+              <div className="links">
+                <a href="github">
+                  <FiGithub />
+                </a>
+                <a href="projectlink">
+                  <FiExternalLink />
+                </a>
+              </div>
+            </div>
+          </li>
+
+          <li className="project">
+            <div className="project_img">
+              <a href="projectlink">
+                <img src={project1} alt="" />
+              </a>
+            </div>
+
+            <div className="project_content">
+              <p className="featured_project">Featured Project</p>
+              <h3 className="project_title">Project Title</h3>
+              <p className="project_description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum,
+                in?
+              </p>
+              <ul className="tags">
+                <li>Vs Code</li>
+                <li>React</li>
+                <li>Node</li>
+              </ul>
+              <div className="links">
+                <a href="github">
+                  <FiGithub />
+                </a>
+                <a href="projectlink">
+                  <FiExternalLink />
+                </a>
+              </div>
+            </div>
+          </li>
+        </ul>
       </div>
     </section>
   );
